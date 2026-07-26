73fc8dd315bf911a3bf293da6f616b2d62db3ba6
Marcus Atta <marcusatta27@gmail.com>
2026-07-26 20:21:38 +0300

    myfirstcommit

---
+++ Iremtable.java
@@ -0,0 +1,22 @@
+<html> lang="en">
+    <head>
+    <title>Today we are going to see if I can remember how to create tables</title>
+    </head>
+    <body>
+<h1> Tables</h1>
+<table border="10">
+    <tr>
+        <th>Portugal</th>
+        <th>Spain</th>
+        <th>France</th>
+        <th>Germany</th>
+        <th>Italy</th>
+    </tr>
+    <tr>
+        <td>Ronaldo</td>
+        <td>Lamine</td>
+        <td>Mbappe</td>
+        <td>Oliver</td>
+        <td>Mattia</td>
+    </body>
+</html>
